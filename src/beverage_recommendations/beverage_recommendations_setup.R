21467854c2f0d552dbba1de8792dbcdc2a558919
Daniel Chen <Maveri2k@yahoo.com>
2015-12-03 00:31:04 -0500

    rename y_axis_range variable, clash with fxn name

--- src/beverage_recommendations/beverage_recommendations_setup.R
+++ src/beverage_recommendations/beverage_recommendations_setup.R
@@ -3,10 +3,10 @@ df <-  data.frame(soda = config_pt_soda_wk,
 df <- melt(df)
 
 df$text <- df$value
-y_axis_range <- y_axis_range(max(df$value), 'beverage_recommendations')
+y_axis_range_calc <- y_axis_range(max(df$value), 'beverage_recommendations')
 
-min_bar_height <- y_axis_range * 0.01
-min_center_bar_height <- y_axis_range * 0.1
+min_bar_height <- y_axis_range_calc * 0.01
+min_center_bar_height <- y_axis_range_calc * 0.1
 df$value <- ifelse(df$value == 0, min_bar_height, df$value)
 df$text_y <- sapply(X = df$text, FUN = y_axis_text_height,
                     min_bar_height, min_center_bar_height, 'beverage_recommendations')
--- src/composite_beverage_servings/composite_beverage_servings_setup.R
+++ src/composite_beverage_servings/composite_beverage_servings_setup.R
@@ -5,9 +5,9 @@ df <- melt(df)
 # df$value <- c(0, 1, 20)
 
 df$text <- df$value
-y_axix_range <- y_axis_range(max(df$value), 'composite_beverage_servings')
-min_bar_height <- y_axix_range * 0.01
-min_center_bar_height <- y_axix_range * 0.1
+y_axis_range_value <- y_axis_range(max(df$value), 'composite_beverage_servings')
+min_bar_height <- y_axis_range_value * 0.01
+min_center_bar_height <- y_axis_range_value * 0.1
 df$value <- ifelse(df$value == 0, min_bar_height, df$value)
 df$text_y <- sapply(X = df$text, FUN = y_axis_text_height,
                     min_bar_height, min_center_bar_height, 'composite_beverage_servings')
