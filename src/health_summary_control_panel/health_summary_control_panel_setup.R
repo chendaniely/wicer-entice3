aad99bcf8e990ba26c8573441c371375587d6320
Daniel Chen <Maveri2k@yahoo.com>
2015-12-07 15:29:50 -0500

    transland health_summary_words

--- src/health_summary_control_panel/health_summary_control_panel_setup.R
+++ src/health_summary_control_panel/health_summary_control_panel_setup.R
@@ -5,4 +5,6 @@ health_summary_colors <- get_health_summary_colors(
     blood_pressure_s_value = config_pt_bp_s,
     blood_pressure_d_value = config_pt_bp_d)
 
-health_summary_words <- lapply(X = health_summary_colors, FUN = get_health_summary_words_from_color)
+health_summary_words <- lapply(X = health_summary_colors,
+                               FUN = get_health_summary_words_from_color,
+                               language=data_config_pt_language)
