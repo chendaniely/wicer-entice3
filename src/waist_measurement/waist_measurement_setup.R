0c1016e5dedbdef2177c9455859500527ed4d746
Daniel Chen <Maveri2k@yahoo.com>
2015-12-10 18:57:47 -0500

    fix waist_higher not being read for text image moved to .R

--- src/waist_measurement/waist_measurement_setup.R
+++ src/waist_measurement/waist_measurement_setup.R
@@ -7,3 +7,9 @@ base_image_g <- get_base_waist_image(gender = config_pt_sex,
                                      language = config_pt_language)
 indicator_g <- create_grob_from_png_dir(
     'EnTICE3_2/Waist Measurement/Waist measurement indicator box.png')
+
+if (tolower(config_pt_sex) == "male" | config_pt_sex == 1) {
+    waist_higher <- 40
+} else if (tolower(config_pt_sex) == "female" | config_pt_sex == 2) {
+    waist_higher <- 35
+}
