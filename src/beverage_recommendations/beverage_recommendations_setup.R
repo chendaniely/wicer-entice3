fde45db75782f85a670afebdb9653f2209c00747
Daniel Chen <Maveri2k@yahoo.com>
2016-01-24 04:00:44 -0500

    beverage recommendation y axis values carring over from previous image

--- src/beverage_recommendations/beverage_recommendations_setup.R
+++ src/beverage_recommendations/beverage_recommendations_setup.R
@@ -5,7 +5,7 @@ df <- melt(df)
 df$text <- df$value
 y_axis_range_calc <- y_axis_range(max(df$value), 'beverage_recommendations')
 
-min_bar_height <- y_axis_range_calc * 0.01
+min_bar_height <- y_axis_range_calc * 0.10
 min_center_bar_height <- y_axis_range_calc * 0.1
 df$value <- ifelse(df$value == 0, min_bar_height, df$value)
 df$text_y <- sapply(X = df$text, FUN = y_axis_text_height,
