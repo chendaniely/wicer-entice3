3a19fb1c120c5741959f701e5627a583dc488bcd
Daniel Chen <Maveri2k@yahoo.com>
2016-01-24 13:48:37 -0500

    risk of bp diastolic arrow placement

--- src/00-batch.R
+++ src/00-batch.R
@@ -15,13 +15,13 @@ pt_english_a1_ids <- c('Sample1', 'Sample4', 'Sample6')[3]
 pt_english_a2_ids <- c('Sample1', 'Sample4', 'Sample6')[3]
 pt_english_b1_ids <- c('Sample1', 'Sample4', 'Sample6')[3]
 pt_english_b2_ids <- c('Sample1', 'Sample4', 'Sample6')[3]
-pt_english_all_ids <- c(pt_english_a1_ids, pt_english_a2_ids, pt_english_b1_ids, pt_english_b2_ids)
+pt_english_all_ids <- c(pt_english_a1_ids, pt_english_a2_ids, pt_english_b1_ids, pt_english_b2_ids)[1]
 
 pt_spanish_a1_ids <- c('Sample2', 'Sample3', 'Sample5', 'Sample6')[4]
 pt_spanish_a2_ids <- c('Sample2', 'Sample3', 'Sample5', 'Sample6')[4]
 pt_spanish_b1_ids <- c('Sample2', 'Sample3', 'Sample5', 'Sample6')[4]
 pt_spanish_b2_ids <- c('Sample2', 'Sample3', 'Sample5', 'Sample6')[4]
-pt_spanish_all_ids <- c(pt_spanish_a1_ids, pt_spanish_a2_ids, pt_spanish_b1_ids, pt_spanish_b2_ids)
+pt_spanish_all_ids <- c(pt_spanish_a1_ids, pt_spanish_a2_ids, pt_spanish_b1_ids, pt_spanish_b2_ids)[1]
 
 
 
