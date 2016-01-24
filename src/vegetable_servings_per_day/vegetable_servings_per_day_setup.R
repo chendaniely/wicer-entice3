877d261b65c54fe659738630ced2257c3e284636
Daniel Chen <Maveri2k@yahoo.com>
2016-01-24 02:09:43 -0500

    vegetable servinds per day minimum to 10%

--- src/vegetable_servings_per_day/vegetable_servings_per_day_setup.R
+++ src/vegetable_servings_per_day/vegetable_servings_per_day_setup.R
@@ -5,7 +5,7 @@ df <- melt(df)
 # df$value <- c(0, 1, 20)
 df$text <- df$value
 y_axix_range <- y_axis_range(max(df$value), 'vegetable_servings_per_day')
-min_bar_height <- y_axix_range * 0.03
+min_bar_height <- y_axix_range * 0.1
 min_center_bar_height <- y_axix_range * 0.1
 df$value <- ifelse(df$value == 0, min_bar_height, df$value)
 df$text_y <- sapply(X = df$text, FUN = y_axis_text_height,
