56ba87d51a860241f693dc1bcbfa907fcd0212e8
Daniel Chen <Maveri2k@yahoo.com>
2016-02-02 20:35:21 -0500

    higher minimum values

--- src/fruit_servings_per_day/fruit_servings_per_day_setup.R
+++ src/fruit_servings_per_day/fruit_servings_per_day_setup.R
@@ -5,7 +5,7 @@ df <- melt(df)
 
 df$text <- df$value
 y_axix_range <- y_axis_range(max(df$value), 'fruit_servings_per_day')
-min_bar_height <- y_axix_range * 0.01
+min_bar_height <- y_axix_range * 0.02
 min_center_bar_height <- y_axix_range * 0.1
 df$value <- ifelse(df$value == 0, min_bar_height, df$value)
 df$text_y <- sapply(X = df$text, FUN = y_axis_text_height,
