75c9267ba6d511661fe7cdc90a98fbb74dba4bc9
Daniel Chen <Maveri2k@yahoo.com>
2016-04-25 10:24:29 -0400

    manual pt ids in the all and figs only variables

--- src/00-01-batch.R
+++ src/00-01-batch.R
@@ -30,11 +30,11 @@ pt_english_b2_ids <- c('Sample1', 'Sample4', 'Sample6')
 source('src/create_english_b2.R')
 
 # English all figures
-pt_english_all_ids <- c(pt_english_a1_ids, pt_english_a2_ids, pt_english_b1_ids, pt_english_b2_ids)[1]
+pt_english_all_ids <- c('Sample2', 'Sample3', 'Sample5', 'Sample6')
 source('src/create_english_figs_only.R')
 
 # English all text and figures
-pt_english_all_ids <- c(pt_english_a1_ids, pt_english_a2_ids, pt_english_b1_ids, pt_english_b2_ids)[1]
+pt_english_all_ids <- c('Sample1', 'Sample4', 'Sample6')
 source('src/create_english_text_and_figs.R')
 
 
@@ -54,9 +54,9 @@ pt_spanish_b2_ids <- c('Sample2', 'Sample3', 'Sample5', 'Sample6')
 source('src/create_spanish_b2.R')
 
 # Spanish all figures
-pt_spanish_all_ids <- c(pt_spanish_a1_ids, pt_spanish_a2_ids, pt_spanish_b1_ids, pt_spanish_b2_ids)[1]
+pt_spanish_all_ids <- c('Sample2', 'Sample3', 'Sample5', 'Sample6')
 source('src/create_spanish_figs_only.R')
 
 # Spanish all text and figures
-pt_spanish_all_ids <- c(pt_spanish_a1_ids, pt_spanish_a2_ids, pt_spanish_b1_ids, pt_spanish_b2_ids)[1]
+pt_spanish_all_ids <- c('Sample2', 'Sample3', 'Sample5', 'Sample6')
 source('src/create_spanish_text_and_figs.R')
